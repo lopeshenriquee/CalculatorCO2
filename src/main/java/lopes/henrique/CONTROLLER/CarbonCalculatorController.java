73e14afafdd5bad62b050ea950a91773433e9188
lopeshenriquee <henriquesantanalopes9@gmail.com>
2024-10-04 00:58:58 -0300

    alteração no retorno dos cálculos (utilizando agr um array)

--- src/main/java/lopes/henrique/CONTROLLER/CarbonCalculatorController.java
+++ src/main/java/lopes/henrique/CONTROLLER/CarbonCalculatorController.java
@@ -1,5 +1,6 @@
 package lopes.henrique.CONTROLLER;
 
+import lopes.henrique.MODEL.CarbonResult;
 import lopes.henrique.MODEL.Energy;
 import lopes.henrique.MODEL.Food;
 import lopes.henrique.MODEL.Transport;
@@ -7,17 +8,13 @@ import lopes.henrique.MODEL.Transport;
 import java.text.DecimalFormat;
 
 public class CarbonCalculatorController {
-    public double calculatorTotalCarbon(double energyConsumption, double distance, double efficiency, String fuelType, int diet){
-        DecimalFormat df = new DecimalFormat("0.00");
-
+    public double[] calculateCarbonValues(double energyConsumption, double distance, double efficiency, String fuelType, int diet){
         double carbonEnergy = Energy.calculateEnergyCarbon(energyConsumption);
         double carbonTransport = Transport.calculateTransportCarbon(distance, efficiency, fuelType);
         double carbonFood = Food.calculateFoodCarbon(diet);
 
-        System.out.println("CO2 gerado por energia: " + df.format(carbonEnergy));
-        System.out.println("CO2 gerado no transporte: " + df.format(carbonTransport));
-        System.out.println("CO2 gerado na sua alimentação: " + df.format(carbonFood));
+        double totalCarbon = carbonEnergy + carbonTransport + carbonFood;
 
-        return carbonEnergy + carbonTransport + carbonFood;
+        return new double[]{totalCarbon, carbonEnergy, carbonTransport, carbonFood};
     }
 }
